79ab1dedf9b0757618635276eeab43f36868a959
Jonny <jzhangfob@gmail.com>
2025-01-26 23:48:32 +0000

    Refactored dim_customers to include 2 staging models. Set project configs to materialize all marts as tables, staging as views

---
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -0,0 +1,4 @@
+select id as customer_id,
+first_name,
+last_name
+from `learn-dbt-449003.jaffle_shop.customers`
---
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -0,0 +1,5 @@
+select id as order_id,
+user_id as customer_id,
+order_date,
+status
+from `learn-dbt-449003.jaffle_shop.orders`
